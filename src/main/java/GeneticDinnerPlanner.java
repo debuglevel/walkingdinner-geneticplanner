692c3e9087c4651f4403ad9789bb922ffde517a9
Marc Kohaupt <marc.kohaupt@gmail.com>
2018-05-12 21:06:59 +0200

    Add geo optimization

--- src/main/java/GeneticDinnerPlanner.java
+++ src/main/java/GeneticDinnerPlanner.java
@@ -5,7 +5,6 @@ import io.jenetics.SwapMutator;
 import io.jenetics.engine.Engine;
 import io.jenetics.engine.EvolutionResult;
 import io.jenetics.engine.EvolutionStatistics;
-import io.jenetics.engine.Limits;
 import io.jenetics.util.ISeq;
 
 public class GeneticDinnerPlanner {
@@ -31,6 +30,7 @@ public class GeneticDinnerPlanner {
 
         Engine<EnumGene<Team>, Double> engine = Engine
                 .builder(problem)
+                .populationSize(500)
                 .optimize(Optimize.MINIMUM)
                 .alterers(new SwapMutator<>(0.15),
                         new PartiallyMatchedCrossover<>(0.15))
@@ -42,7 +42,7 @@ public class GeneticDinnerPlanner {
         final EvolutionResult<EnumGene<Team>, Double> result = engine.stream()
 //                .limit(Limits.bySteadyFitness(40_000))
                 // TODO: right now, jenetics seems to have a bug which discards the final best result: https://github.com/jenetics/jenetics/issues/343
-                .limit(Limits.byFitnessThreshold(0d))
+//                .limit(Limits.byFitnessThreshold(0d))
                 .peek(g -> {
                     if (g.getGeneration() % 500 == 0) {
                         System.out.println("Generation: " + g.getGeneration() + "\t| Best Fitness: " + g.getBestFitness());
