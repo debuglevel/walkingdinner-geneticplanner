3fe89ac9dd2c5a3c4fb05ac815bc069c3227e363
Marc Kohaupt <marc.kohaupt@gmail.com>
2018-05-13 15:26:54 +0200

    Print version information on startup

--- src/main/kotlin/rocks/huwi/walkingdinner/geneticplanner/Main.kt
+++ src/main/kotlin/rocks/huwi/walkingdinner/geneticplanner/Main.kt
@@ -1,5 +1,6 @@
 package rocks.huwi.walkingdinner.geneticplanner
 
 fun main(args: Array<String>) {
+    println("=== ${BuildVersion.buildTitle} ${BuildVersion.buildVersion} ===")
     GeneticPlanner().run()
 }
