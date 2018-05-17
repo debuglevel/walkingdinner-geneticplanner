59a9cd13c0bea682c24e63b6d89f542c62196367
Marc Kohaupt <marc.kohaupt@gmail.com>
2018-05-17 17:43:07 +0200

    Save already calculated distances to map

--- src/main/kotlin/rocks/huwi/walkingdinner/geneticplanner/GeoUtils.kt
+++ src/main/kotlin/rocks/huwi/walkingdinner/geneticplanner/GeoUtils.kt
@@ -1,18 +1,33 @@
 package rocks.huwi.walkingdinner.geneticplanner
 
 object GeoUtils {
+    private val distances = mutableMapOf<GeoPair, Double>()
+
     private const val AVERAGE_RADIUS_OF_EARTH_KM = 6371.0
     fun calculateDistanceInKilometer(sourceLat: Double, sourceLng: Double,
                                      destinationLat: Double, destinationLng: Double): Double {
 
-        val latDistance = Math.toRadians(sourceLat - destinationLat)
-        val lngDistance = Math.toRadians(sourceLng - destinationLng)
+        val pair = GeoPair(sourceLat, sourceLng, destinationLat, destinationLng)
+        var distance = distances[pair]
+        if (distance != null)
+        {
+            return distance
+        }else{
+            val latDistance = Math.toRadians(sourceLat - destinationLat)
+            val lngDistance = Math.toRadians(sourceLng - destinationLng)
 
-        val a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) + (Math.cos(Math.toRadians(sourceLat)) * Math.cos(Math.toRadians(destinationLat))
-                * Math.sin(lngDistance / 2) * Math.sin(lngDistance / 2))
+            val a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) + (Math.cos(Math.toRadians(sourceLat)) * Math.cos(Math.toRadians(destinationLat))
+                    * Math.sin(lngDistance / 2) * Math.sin(lngDistance / 2))
 
-        val c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+            val c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
 
-        return AVERAGE_RADIUS_OF_EARTH_KM * c
+            distance = AVERAGE_RADIUS_OF_EARTH_KM * c
+            distances[pair] = distance
+            return distance
+        }
+    }
+
+    data class GeoPair(val sourceLat: Double, val sourceLng: Double,
+                        val destinationLat: Double, val destinationLng: Double) {
     }
 }
