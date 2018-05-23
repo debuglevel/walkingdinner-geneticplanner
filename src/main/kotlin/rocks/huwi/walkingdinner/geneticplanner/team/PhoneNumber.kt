21e35641231191f8f5c53ac9be9a57e94b3b5ba4
Marc Kohaupt <marc.kohaupt@gmail.com>
2018-05-23 17:18:15 +0200

    Refactoring

--- src/main/kotlin/rocks/huwi/walkingdinner/geneticplanner/team/PhoneNumber.kt
+++ src/main/kotlin/rocks/huwi/walkingdinner/geneticplanner/team/PhoneNumber.kt
@@ -6,16 +6,12 @@ import com.opencsv.bean.AbstractBeanField
 import com.opencsv.exceptions.CsvDataTypeMismatchException
 
 data class PhoneNumber(private val number: String) {
-    private val formattedNumber: String
-
-    init {
-        formattedNumber = try {
-            val phoneUtil = PhoneNumberUtil.getInstance()
-            val numberProto = phoneUtil.parse(number, "DE")
-            phoneUtil.format(numberProto, PhoneNumberUtil.PhoneNumberFormat.INTERNATIONAL)
-        } catch (e: NumberParseException) {
-            number
-        }
+    private val formattedNumber: String = try {
+        val phoneUtil = PhoneNumberUtil.getInstance()
+        val numberProto = phoneUtil.parse(number, "DE")
+        phoneUtil.format(numberProto, PhoneNumberUtil.PhoneNumberFormat.INTERNATIONAL)
+    } catch (e: NumberParseException) {
+        number
     }
 
     override fun toString(): String {
