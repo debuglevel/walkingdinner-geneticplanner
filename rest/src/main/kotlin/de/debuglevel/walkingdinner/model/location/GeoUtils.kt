949490a76340f22a84f620cd6daf4663388b6f9f
Marc Kohaupt <debuglevel@gmail.com>
2019-09-11 10:43:34 +0200

    Document cache

--- rest/src/main/kotlin/de/debuglevel/walkingdinner/model/location/GeoUtils.kt
+++ rest/src/main/kotlin/de/debuglevel/walkingdinner/model/location/GeoUtils.kt
@@ -6,18 +6,25 @@ import kotlin.math.sin
 import kotlin.math.sqrt
 
 object GeoUtils {
+    /**
+     * Cache for already calculated distances
+     * TODO: hopefully, accessing the cache is faster than calculating the distance; but noting microbenchmark results would be nice
+     */
     private val distances = hashMapOf<Pair<Location, Location>, Double>()
 
     private const val AVERAGE_RADIUS_OF_EARTH_KM = 6371.0
+
     fun calculateDistanceInKilometer(
         source: Location,
         destination: Location
     ): Double {
+        // Retrieve distance from cache if already calculated
         val pair = Pair(source, destination)
         var distance = distances[pair]
         return if (distance != null) {
             distance
         } else {
+            // Calculate distance if not in cache
             val latDistance = Math.toRadians(source.lat - destination.lat)
             val lngDistance = Math.toRadians(source.lng - destination.lng)
 
@@ -36,7 +43,10 @@ object GeoUtils {
             val c = 2 * atan2(sqrt(a), sqrt(1 - a))
 
             distance = AVERAGE_RADIUS_OF_EARTH_KM * c
+
+            // Put calculation into cache
             distances[pair] = distance
+
             distance
         }
     }
