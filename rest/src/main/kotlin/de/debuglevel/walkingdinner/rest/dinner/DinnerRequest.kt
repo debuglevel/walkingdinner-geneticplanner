db678437e19d0a8d094d02732adeeba0c13b5b73
Marc Kohaupt <debuglevel@gmail.com>
2019-09-12 18:33:46 +0200

    Remove @JsonFormat on DateTime, as it's probably better to use the "2019-12-24T23:59:59" format

--- rest/src/main/kotlin/de/debuglevel/walkingdinner/rest/dinner/DinnerRequest.kt
+++ rest/src/main/kotlin/de/debuglevel/walkingdinner/rest/dinner/DinnerRequest.kt
@@ -1,11 +1,9 @@
 package de.debuglevel.walkingdinner.rest.dinner
 
-import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDateTime
 
 data class DinnerRequest(
     val name: String,
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     val beginDateTime: LocalDateTime
 )
