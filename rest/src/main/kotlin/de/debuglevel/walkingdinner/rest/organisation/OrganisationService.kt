46725e2c7793c19c7959c8bcfc24a75a8ca75409
Marc Kohaupt <debuglevel@gmail.com>
2019-11-18 19:28:11 +0100

    Adjust OrganisationService to other Services

--- rest/src/main/kotlin/de/debuglevel/walkingdinner/rest/organisation/OrganisationService.kt
+++ rest/src/main/kotlin/de/debuglevel/walkingdinner/rest/organisation/OrganisationService.kt
@@ -1,6 +1,5 @@
 package de.debuglevel.walkingdinner.rest.organisation
 
-import de.debuglevel.walkingdinner.rest.common.ElementNotFoundException
 import mu.KotlinLogging
 import java.util.*
 import javax.inject.Singleton
@@ -11,37 +10,26 @@ class OrganisationService(
 ) {
     private val logger = KotlinLogging.logger {}
 
-    /**
-     * @throws ElementNotFoundException if no element with the given id exists
-     */
     fun get(id: UUID): Organisation {
         logger.debug { "Getting organisation with ID '$id'..." }
-
-        val organisation = organisationRepository.findById(id).orElseThrow {
-            ElementNotFoundException(
-                id
-            )
-        }
-
-        logger.debug { "Got organisation with ID '$id': $organisation" }
+        val organisation = organisationRepository.findById(id).orElseThrow { OrganisationNotFoundException(id) }
+        logger.debug { "Got organisation: $organisation" }
         return organisation
     }
 
     fun getAll(): Set<Organisation> {
         logger.debug { "Getting all organisations..." }
-
         val organisations = organisationRepository.findAll().toSet()
-
         logger.debug { "Got all organisations" }
         return organisations
     }
 
     fun save(organisation: Organisation): Organisation {
-        logger.debug { "Saving organisation..." }
-
+        logger.debug { "Saving organisation '$organisation'..." }
         val savedOrganisation = organisationRepository.save(organisation)
-
-        logger.debug { "Saved organisation" }
+        logger.debug { "Saved organisation: $savedOrganisation" }
         return savedOrganisation
     }
+
+    class OrganisationNotFoundException(organisationId: UUID) : Exception("Organisation $organisationId not found")
 }
